d5612a746206a15159554cd115f22f0735d926c5
Lyuba Eremeeva <leremeeva174@gmail.com>
2018-03-05 01:30:18 +0300

    components moved to "components" dir

---
+++ src/app/components/course/course.service.ts
@@ -0,0 +1,76 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {Course} from '../../model/course';
+import {catchError} from 'rxjs/operators';
+import {of} from 'rxjs/observable/of';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable()
+export class CourseService {
+
+  private courseUrl = 'http://localhost:3000/course';
+
+  constructor(private http: HttpClient) {
+  }
+
+  /** GET courses from the server */
+  getCourses(): Observable<Course[]> {
+    return this.http
+      .get<Course[]>(this.courseUrl)
+      .pipe(
+        catchError(this.handleError('getCourses', []))
+      );
+  }
+
+  /** GET course by id. Will 404 if id not found */
+  getCourse(id: number): Observable<Course> {
+    const url = `${this.courseUrl}/${id}`;
+    return this.http
+      .get<Course>(url)
+      .pipe(
+        catchError(this.handleError<Course>(`getCourse id=${id}`))
+      );
+  }
+
+  /** POST: create a new course to the server */
+  addCourse(course: Course): Observable<Course> {
+    return this.http
+      .post<Course>(this.courseUrl, course, httpOptions)
+      .pipe(
+        catchError(this.handleError<Course>('addCourse'))
+      );
+  }
+
+  /** DELETE: delete the course from the server */
+  deleteCourse(id: number): Observable<Course> {
+    const url = `${this.courseUrl}/${id}`;
+
+    return this.http
+      .delete<Course>(url, httpOptions)
+      .pipe(
+        catchError(this.handleError<Course>('deleteCourse'))
+      );
+  }
+
+  /** PUT: update the hero on the server */
+  updateAddress(course: Course): Observable<Course> {
+    return this.http
+      .put(this.courseUrl, course, httpOptions)
+      .pipe(
+        catchError(this.handleError<any>('updateCourse'))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
---
+++ src/app/components/student/student.service.ts
@@ -0,0 +1,76 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import {Student} from '../../model/student';
+import {catchError} from 'rxjs/operators';
+import {of} from 'rxjs/observable/of';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable()
+export class StudentService {
+
+  private studentUrl = 'http://localhost:3000/student';
+
+  constructor(private http: HttpClient) {
+  }
+
+  /** GET students from the server */
+  getStudents(): Observable<Student[]> {
+    return this.http
+      .get<Student[]>(this.studentUrl)
+      .pipe(
+        catchError(this.handleError('getStudents', []))
+      );
+  }
+
+  /** GET student by id. Will 404 if id not found */
+  getStudent(id: number): Observable<Student> {
+    const url = `${this.studentUrl}/${id}`;
+    return this.http
+      .get<Student>(url)
+      .pipe(
+        catchError(this.handleError<Student>(`getStudent id=${id}`))
+      );
+  }
+
+  /** POST: create a new student to the server */
+  addStudent(student: Student): Observable<Student> {
+    return this.http
+      .post<Student>(this.studentUrl, student, httpOptions)
+      .pipe(
+        catchError(this.handleError<Student>('addStudent'))
+      );
+  }
+
+  /** DELETE: delete the student from the server */
+  deleteStudent(id: number): Observable<Student> {
+    const url = `${this.studentUrl}/${id}`;
+
+    return this.http
+      .delete<Student>(url, httpOptions)
+      .pipe(
+        catchError(this.handleError<Student>('deleteStudent'))
+      );
+  }
+
+  /** PUT: update the hero on the server */
+  updateAddress(student: Student): Observable<Student> {
+    return this.http
+      .put(this.studentUrl, student, httpOptions)
+      .pipe(
+        catchError(this.handleError<any>('updateStudent'))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
