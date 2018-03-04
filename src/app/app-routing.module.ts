17fe0b1e70214ef1293e84811ef0cee412b63cf0
Lyuba Eremeeva <leremeeva174@gmail.com>
2018-03-05 01:37:50 +0300

    added routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {CourseComponent} from './components/course/course.component';
+import {TeacherComponent} from './components/teacher/teacher.component';
+import {StudentComponent} from './components/student/student.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: 'courses', component: CourseComponent},
+  {path: 'teachers', component: TeacherComponent},
+  {path: 'students', component: StudentComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {StudentComponent} from './components/student/student.component';
 import {TeacherComponent} from './components/teacher/teacher.component';
 import {CourseComponent} from './components/course/course.component';
+import {AppRoutingModule} from './app-routing.module';
+import {CourseService} from './components/course/course.service';
+import {StudentService} from './components/student/student.service';
+import {TeacherService} from './components/teacher/teacher.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -21,9 +26,14 @@ import {CourseComponent} from './components/course/course.component';
     TeacherComponent,
     CourseComponent
   ],
-  providers: [],
+  providers: [
+    CourseService,
+    StudentService,
+    TeacherService
+  ],
   bootstrap: [
     AppComponent
   ]
 })
-export class AppModule { }
+export class AppModule {
+}
