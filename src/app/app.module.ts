234470b81d38a45be376f428b842202bf5e9b7fe
liye0717 <liubou.yerameyeva@netcracker.com>
2018-03-04 22:15:33 +0300

    angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {StudentComponent} from './student/student.component';
+import {TeacherComponent} from './teacher/teacher.component';
+import {CourseComponent} from './course/course.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    AppComponent,
+    StudentComponent,
+    TeacherComponent,
+    CourseComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/student/student.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './address.component.html',
+  styleUrls: ['./address.component.less']
+})
+export class StudentComponent {
+}
